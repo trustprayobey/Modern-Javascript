e6426cbfa8a5b4c496e7a409f2e6f7e0af47ea3c
Brian Rono <brishadow24@gmail.com>
2022-07-31 20:06:43 +0300

    introducing objects and this keyword

--- es6.js
+++ es6.js
@@ -3,10 +3,38 @@
 //also block but for constants(read-only)
 //var -> function
 //let -> block
-function sayHello() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i) //Es6 'let' is only accessible in the block
-    }
-    console.log(i) //var is accessible here and in the block
+// function sayHello() {
+//     for (var i = 0; i < 5; i++) {
+//         console.log(i) //Es6 'let' is only accessible in the block
+//     }
+//     console.log(i) //var is accessible here and in the block
+// }
+// sayHello() //use c'onst' over 'let' and 'let' only when reasinging throw away var
+
+
+//objects
+// const person = {
+//     name: 'Brian',
+//     walk: function () { },
+//     talk() { } //same method es6 way
+// }
+// person.walk()
+// person['name'] //same but es6
+
+// const targetMember = 'name'
+// person[targetMember] = 'Ian'
+
+
+//this keyword
+
+const person = {
+    name: "Mosh",
+    walk() { console.log(this) }
 }
-sayHello() //use c'onst' over 'let' and 'let' only when reasinging throw away var
+person.walk() //this returns reference to the object
+
+
+const walk = person.walk
+console.log(walk)
+// we can not call walk
+//when use this outside of the object it returns a global object which is the window
